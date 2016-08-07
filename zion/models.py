c48a396219e4dd51d837af8460f2c4d894694ba6
Razin Tanvir <tanvirrazin@gmail.com>
2016-08-08 01:31:50 +0600

    Removed unnecessary models

--- zion/models.py
+++ zion/models.py
@@ -1,17 +1,3 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
-
-STATUS_CHOICES = (
-    ('w', 'White Listed'),
-    ('b', 'Black Listed'),
-)
-
-class ClientIP(models.Model):
-    ip_address = models.GenericIPAddressField()
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
-
-    def __unicode__(self):
-        return self.ip_address
-
--- zion/views.py
+++ zion/views.py
@@ -1,14 +1,11 @@
-from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from django.core.cache import cache
 import requests
 
-from .models import ClientIP
-
 def call_third_party():
     response = requests.get('https://www.zipcodeapi.com/rest/kpZVAfW18keQsK9j8NGNFiJroM4EFyOZXOVx4SycdN3FxcYG0FtGXc7mZb0FKPFH/info.json/90210/degrees')
-    return HttpResponse(response)
+    return JsonResponse(response.json())
 
 def trinity_view(request):
     client_ip = request.META.get('REMOTE_ADDR')
@@ -19,13 +16,14 @@ def trinity_view(request):
             return call_third_party()
         else:
             return JsonResponse({'detail': 'Permission Denied.'})
+
     else:
         g_recaptcha_response = request.POST.get('g-recaptcha-response', [])
 
         if len(g_recaptcha_response) > 0 and g_recaptcha_response[0] != '':
-
             cache.set('{}_status'.format(client_ip), 'w')
             return call_third_party()
+
         else:
             template = loader.get_template('trinity_template.html')
             context = {'name': request.GET.get('name', '')}
