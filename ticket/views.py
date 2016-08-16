7d7fba92da8323dbc2ec23b36f7e95b33e2c69e8
Razin Tanvir <tanvirrazin@gmail.com>
2016-08-16 23:13:26 +0600

    Added new functionalities

--- ticket/views.py
+++ ticket/views.py
@@ -6,6 +6,18 @@ from django.conf import settings
 import requests
 
 
+def check_captcha(captcha_response, remote_ip):
+    post_data = {
+        "secret": settings.RECAPTCHA_SECRETKEY,
+        "response": captcha_response,
+        "remoteip": remote_ip
+    }
+
+    response = requests.post('https://www.google.com/recaptcha/api/siteverify', data=post_data)
+    if response.json()['success'] == True:
+        return True
+    return False
+
 def build_soap_request_body(data):
     data_string = ""
     for key, value in data.items():
@@ -42,12 +54,15 @@ def ticket_service_view(request):
 
     else:
         g_recaptcha_response = request.POST.get('g-recaptcha-response', [])
-
-        if len(g_recaptcha_response) > 0 and g_recaptcha_response[0] != '':
+        if g_recaptcha_response != '' and check_captcha(g_recaptcha_response, client_ip):
             cache.set('{}_status'.format(client_ip), 'w')
             return HttpResponse('You are allowed to call the service now.')
 
         else:
-            template = loader.get_template('ticket_template.html')
-            context = {'site_key': settings.RECAPTCHA_SITEKEY}
-            return HttpResponse(template.render(context, request))
+            unathorised_request_num = cache.get_or_set('{}_unathorised_request_num'.format(client_ip), 0)
+            if unathorised_request_num < settings.MAX_UNATHORISED_REQUEST_NUM:
+                cache.incr('{}_unathorised_request_num'.format(client_ip))
+                template = loader.get_template('ticket_template.html')
+                context = {'site_key': settings.RECAPTCHA_SITEKEY}
+                return HttpResponse(template.render(context, request))
+            return HttpResponse('You are not allowed to access this service anymore.')
